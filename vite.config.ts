8e100243bc4401e8923b73f01a82a44da417897e
BaoTQ <qbaok62@gmail.com>
2024-11-05 21:05:19 +0700

    update vite config build output

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     open: true,
   },
   build: {
-    outDir: "../dist", // Output in the dist/ folder
+    outDir: "./dist", // Output in the dist/ folder
     emptyOutDir: true, // Empty the folder first
     sourcemap: true, // Add sourcemap
   },
